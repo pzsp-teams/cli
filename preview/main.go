2aabf44eab711fc318fe8d8c45530e11e38d1905
Damian D'Souza <dsouzadamian189@gmail.com>
2025-12-05 20:47:19 +0100

    feat: adjust preview to use global MultiLogger

--- preview/main.go
+++ preview/main.go
@@ -4,23 +4,50 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/pzsp-teams/cli/internal/initializers"
 	"github.com/pzsp-teams/cli/internal/logger"
 )
+
+func init() {
+	verbose := false
+	for _, arg := range os.Args {
+		if arg == "-v" || arg == "--verbose" {
+			verbose = true
+			break
+		}
+	}
+
+	logFile, err := os.Create("preview.log")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create log file: %v\n", err)
+		os.Exit(1)
+	}
+
+	stderrLevel := logger.LevelError
+	if verbose {
+		stderrLevel = logger.LevelDebug
+	}
+
+	initializers.InitMultiOutputLogger(initializers.MultiOutputConfig{
+		StderrLevel:         stderrLevel,
+		FileLevel:           logger.LevelDebug,
+		FileWriter:          logFile,
+		StderrOmitTimestamp: !verbose,
+		FileOmitTimestamp:   false,
+	})
+}
 
 func main() {
 	charmDemo()
 }
 
 func charmDemo() {
-	log := logger.NewCharmFromConfig(logger.DefaultConfig())
-
-	log.Info("=== Basic Logging ===")
-	log.Debug("this is a debug message (won't show with default config)")
+	log := initializers.Logger
+	log.Debug("this is a debug message")
 	log.Info("application started successfully")
 	log.Warn("this is a warning", "code", "WARN001")
 	log.Error("this is an error", "error", "something went wrong")
 
-	fmt.Println("")
 	log.Info("=== Logging with Context (With) ===")
 	userLogger := log.With("user_id", "12345", "role", "admin")
 	userLogger.Info("user logged in")
@@ -30,27 +57,8 @@ func charmDemo() {
 	requestLogger.Info("processing request", "endpoint", "/api/users")
 	requestLogger.Warn("slow request detected", "duration_ms", 2500)
 
-	fmt.Println("")
-	log.Info("=== Debug Level ===")
-	debugLog := logger.NewCharmFromConfig(&logger.Config{
-		Level:  logger.LevelDebug,
-		Format: logger.FormatText,
-		Output: os.Stdout,
-	})
-	debugLog.Debug("now you can see debug messages")
-	debugLog.Info("info level message")
-	debugLog.Warn("warning level message")
-	debugLog.Error("error level message")
-
-	fmt.Println("")
-	noTimestampLog := logger.NewCharmFromConfig(&logger.Config{
-		Level:         logger.LevelInfo,
-		Format:        logger.FormatText,
-		Output:        os.Stdout,
-		OmitTimestamp: true,
-	})
-	noTimestampLog.Info("=== Logs Without Timestamps ===")
-	noTimestampLog.Info("clean log without timestamp")
-	noTimestampLog.Warn("warning without timestamp", "reason", "demonstration")
-	noTimestampLog.Error("error without timestamp", "code", "ERR001")
+	log.Debug("debug level message")
+	log.Info("info level message")
+	log.Warn("warning level message")
+	log.Error("error level message")
 }
