6c3150e7750e1086fa3f97e8a18c342a7e92cc50
Damian D'Souza <dsouzadamian189@gmail.com>
2025-12-06 12:15:26 +0100

    feat(message): implement file parsers to read message data

---
+++ internal/messages/yaml.go
@@ -0,0 +1,20 @@
+package parser
+
+import (
+	"io"
+
+	"gopkg.in/yaml.v3"
+)
+
+// YAMLParser implements Parser for YAML format
+type YAMLParser struct{}
+
+// Parse reads YAML-formatted message data
+func (p *YAMLParser) Parse(r io.Reader) (map[string]MessageData, error) {
+	var messages map[string]MessageData
+	decoder := yaml.NewDecoder(r)
+	if err := decoder.Decode(&messages); err != nil {
+		return nil, err
+	}
+	return messages, nil
+}
